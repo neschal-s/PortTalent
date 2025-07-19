e5d7d75fa654e3c5a95b03c73d7d32032ba77192
Neschal singh <singhneschal@gmail.com>
2025-07-19 16:40:44 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,54 @@
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import './App.css'
+import { Button } from '@/components/ui/button'
+import AppLayout from './layouts/app-layouts'
+import LandingPage from './pages/landing'
+import OnBoarding from './pages/onBoarding'
+import JobPage from './pages/job'
+import JobListing from './pages/job-listing'
+import PostJob from './pages/post-job'
+import SavedJobs from './pages/saved-job'
+import MyJobs from './pages/my-jobs'
+
+
+const router=createBrowserRouter([{
+  element:<AppLayout/>,
+  children:[
+    {
+      path:'/',
+      element:<LandingPage/>
+    },{
+      path:'/onboarding',
+      element:<OnBoarding/>
+    }
+    ,{
+      path:'/jobs',
+      element:<JobListing/>
+    }
+    ,{
+      path:'/job/:id',
+      element:<JobPage/>
+    }
+    ,{
+      path:'/post-job',
+      element:<PostJob/>
+    }
+    ,{
+      path:'/saved-jobs',
+      element:<SavedJobs/>
+    }
+    ,{
+      path:'/my-jobs',
+      element:<MyJobs/>
+    },
+  ]
+}])
+
+
+function App() {
+  return (
+    <RouterProvider router={router}/>
+  )
+}
+
+export default App
